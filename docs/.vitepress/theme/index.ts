a8c741f4d25322d9f3bfdf6d826b67b97442dfb4
喵锵 <mcyyds1234@outlook.com>
2025-07-26 16:25:53 +0800

    主题修改
    来自 hitokoto-developer-docs
    实现了：
    - 平滑的日间/夜间模式切换动画
    - 返回顶部按钮及其动画效果（已改进为透明度淡入淡出效果）
    - 窄屏幕下顶部页面导航（本页目录）的样式优化
    - 页面重载提示组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,22 +2,30 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+// Nprogress
+import vitepressNprogress from 'vitepress-plugin-nprogress'
+import 'vitepress-plugin-nprogress/lib/css/index.css'
+import Layout from './Layout.vue'
 import ImagePreview from './components/ImagePreview.vue'
+import ReloadPrompt from './components/ReloadPrompt.vue'
 import './style.css'
 import './blockquote.css'
 import './custom-block.css'
 import './custom.css'
 import './secret.css'
+import './responsive.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
+    return h(Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'layout-bottom': () => h(ImagePreview)
+      'layout-bottom': () => [h(ImagePreview), h(ReloadPrompt)]
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    app.component('ImagePreview', ImagePreview)
+  enhanceApp(ctx) {
+    vitepressNprogress(ctx)
+    ctx.app.component('ImagePreview', ImagePreview)
+    ctx.app.component('ReloadPrompt', ReloadPrompt)
   }
 } satisfies Theme
